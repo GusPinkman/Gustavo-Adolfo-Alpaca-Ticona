7dc945806fd0514a925bf31e6a3979ae0988c758
GusPinkman <gusadolfoat@gmail.com>
2024-10-15 23:13:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ document.getElementById('menuToggle').addEventListener('click', function() {
         sidebar.style.left = '-250px';
     }
 });
+
+// Obtener todos los enlaces del menú
+const menuLinks = document.querySelectorAll('#sidebar a');
+
+// Obtener todas las secciones
+const sections = document.querySelectorAll('.section-content');
+
+// Función para ocultar todas las secciones
+function hideAllSections() {
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+}
+
+// Añadir evento a cada enlace del menú
+menuLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        // Ocultar todas las secciones
+        hideAllSections();
+        // Mostrar la sección correspondiente
+        const target = this.getAttribute('href').substring(1);
+        document.getElementById(target).classList.add('active');
+        // Cerrar el menú
+        document.getElementById('sidebar').style.left = '-250px';
+    });
+});
